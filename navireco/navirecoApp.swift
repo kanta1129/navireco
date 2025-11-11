74870aea9d1b31d0837892d41746a9136b0f473f
kanta1129 <kanta.f0523@gmail.com>
2025-11-12 02:32:05 +0900

    Initial Commit

---
+++ navireco/navirecoApp.swift
@@ -0,0 +1,17 @@
+//
+//  navirecoApp.swift
+//  navireco
+//
+//  Created by 藤井幹太 on 2025/11/12.
+//
+
+import SwiftUI
+
+@main
+struct navirecoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ navirecoTests/navirecoTests.swift
@@ -0,0 +1,17 @@
+//
+//  navirecoTests.swift
+//  navirecoTests
+//
+//  Created by 藤井幹太 on 2025/11/12.
+//
+
+import Testing
+@testable import navireco
+
+struct navirecoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
